499db19d903f10c65a883c168c24c59103649677
Ei Maung <eimg@fairwayweb.com>
2021-01-09 15:49:50 +0630

    nativebase

--- TodoNative/App.js
+++ TodoNative/App.js
@@ -1,69 +1,58 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Container, Text, Header, Title, Left, Body, Right, Tabs, Tab, List, ListItem } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
 
-const styles = StyleSheet.create({
-	container: {
-		backgroundColor: '#fff',
-	},
-	appbar: {
-		backgroundColor: '#58e',
-		padding: 30
-	},
-	title: {
-		fontSize: 30,
-		fontWeight: "bold",
-		color: "white"
-	},
-	item: {
-		padding: 25,
-		borderBottomColor: '#ccc',
-		borderBottomWidth: 1,
-		flexDirection: 'row'
-	},
-	itemText: {
-		fontSize: 25,
-		flex: 1
-	},
-	input: { 
-		borderColor: 'gray',
-		backgroundColor: '#ffd',
-		padding: 20,
-		borderWidth: 1,
-		borderColor: '#58e',
-		fontSize: 30
+const App = () => {
+	let [ready, setReady] = useState(false);
+
+	async function loadFonts() {
+		await Font.loadAsync({
+			Roboto: require('native-base/Fonts/Roboto.ttf'),
+			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+			...Ionicons.font,
+		});
+		setReady(true);
 	}
-});
 
-function App() {
-	let [ items, setItem ] = useState([
-		{ name: "Apple", price: 8.99 },
-		{ name: "Orange", price: 8.99 },
-		{ name: "Mango", price: 8.99 },
-	]);
+	useEffect(() => {
+		loadFonts();
+	});
 
-	const remove = name => () => {
-		setItem(items.filter(item => item.name !== name));
-	}
+	if(!ready) return <AppLoading />;
 
 	return (
-		<View style={styles.container}>
-			<View style={styles.appbar}>
-				<Text style={styles.title}>Native Toto</Text>
-			</View>
-			<View style={styles.list}>
-				{items.map(item => {
-					return (
-						<View style={styles.item}>
-							<Text style={styles.itemText}>{item.name}</Text>
-							<Button title="Del" onPress={remove(item.name)} />
-						</View>
-					)
-				})}
-			</View>
-			<View>
-				<TextInput style={styles.input} />
-			</View>
-		</View>
+		<Container>
+			<Header>
+				<Left>
+					<Ionicons name="list-circle" size={32} color="white" />
+				</Left>
+				<Body>
+					<Title>Todo Native</Title>
+				</Body>
+				<Right>
+					<Ionicons name="ios-ellipsis-vertical" size={20} color="white" />
+				</Right>
+			</Header>
+			<Tabs>
+				<Tab heading="Tab1">
+					<List>
+						<ListItem><Text>Item One</Text></ListItem>
+						<ListItem><Text>Item Two</Text></ListItem>
+						<ListItem><Text>Item Three</Text></ListItem>
+						<ListItem><Text>Item Four</Text></ListItem>
+						<ListItem><Text>Item Five</Text></ListItem>
+					</List>
+				</Tab>
+				<Tab heading="Tab2">
+					<Text>Tab 2</Text>
+				</Tab>
+				<Tab heading="Tab3">
+					<Text>Tab 3</Text>
+				</Tab>
+			</Tabs>
+		</Container>
 	);
 }
 
